f04f402f7e4d7372422af6daaafeb1e7f9b88d89
bogdanpasterak <bogdanpasterak@gmail.com>
2019-04-24 09:53:00 +0100

    Init model and view, start

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 (function(){
     let model = {
-
+        init : function() {
+            console.log('init model');
+        }
     };
+
     let view = {
-
+        init : function() {
+            console.log('init view');
+        }
     };
+
     let controller = {
         init : function() {
-
+            model.init();
+            view.init();
             console.log('start');
         }
 
     };
+
     controller.init();
 })();
