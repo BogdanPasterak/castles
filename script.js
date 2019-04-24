037e963e565be0d64f9ab3ad896c6af0e4613972
bogdanpasterak <bogdanpasterak@gmail.com>
2019-04-24 15:54:47 +0100

    Build board

--- script.js
+++ script.js
@@ -10,23 +10,44 @@ class Field {
 
 (function(){
     let model = {
-        boadr_width : 4,
-        boadr_height : 4,
+        boadr_width : Number,
+        boadr_height : Number,
+        fields : [],
         init : function() {
+            model.boadr_height = 4;
+            model.boadr_width = 4;
+            for (let row = 0; row < model.boadr_height; row++) {
+                for (let col = 0; col < model.boadr_width; col++) {
+                    model.fields.push(new Field(row, col));   
+                }
+            }
             console.log(model);
         }
     };
 
     let view = {
-        init : function() {
-            console.log('init view');
+        board : Element,
+
+        init : function(b_height, b_width) {
+            this.board = document.querySelector('.board');
+            for (let row = 0; row < b_height; row++) {
+                let rowNode = document.createElement('div');
+                rowNode.classList.add('row');
+                for (let col = 0; col < b_width; col++) {
+                    let fieldNode = document.createElement('div');
+                    fieldNode.classList.add('field');
+                    rowNode.append(fieldNode);
+                }
+                this.board.append(rowNode);  
+            }
+            console.log(this.board);
         }
     };
 
     let controller = {
         init : function() {
             model.init();
-            view.init();
+            view.init(model.boadr_height, model.boadr_width);
             console.log('start');
         }
 
