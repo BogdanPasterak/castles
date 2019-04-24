8e475466a2a8fc290a595d49b42a2d201d6dd3d1
bogdanpasterak <bogdanpasterak@gmail.com>
2019-04-24 09:41:33 +0100

    VMC

---
+++ script.js
@@ -0,0 +1,16 @@
+(function(){
+    let model = {
+
+    };
+    let view = {
+
+    };
+    let controller = {
+        init : function() {
+
+            console.log('start');
+        }
+
+    };
+    controller.init();
+})();
