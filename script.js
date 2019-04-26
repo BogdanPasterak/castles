4f63acd48199cefba4d315ecf193c1b726797e9a
bogdanpasterak <bogdanpasterak@gmail.com>
2019-04-26 15:06:20 +0100

    First test

--- script.js
+++ script.js
@@ -13,19 +13,48 @@ class Field {
         player : Number,
         board_width : Number,
         board_height : Number,
-        fields : [],
+        size : Number,
+        owners : [],
         
         // initialize model
         init : function() {
             this.player = 1;
             this.board_height = 4;
             this.board_width = 4;
-            for (let row = 0; row < this.board_height; row++) {
-                for (let col = 0; col < this.board_width; col++) {
-                    this.fields.push(new Field(row, col));   
-                }
+            this.size = this.board_height * this.board_width;
+            this.owners = new Array(this.size).fill(0);  
+            //console.log(model);
+        },
+
+        // return array affect on fields
+        getAffects : function (arr) {
+            let affects = new Array(this.size).fill(0);
+
+            for (let index = 0; index < this.size; index++) {
+                if (index >= this.board_width) 
+                    affects[index] += arr[index - this.board_width];
+                if (index % this.board_width > 0)
+                    affects[index] += arr[index - 1];
+                if (index % this.board_width < this.board_width - 1)
+                    affects[index] += arr[index + 1];
+                if (index < this.size - this.board_width)
+                    affects[index] += arr[index + this.board_width];
             }
-            console.log(model);
+            return affects;
+        },
+
+        // return array of index neighbors
+        getNeighbors : function(index) {
+            let n = [];
+            if (index >= this.board_width)
+                n.push(index - this.board_width);
+            if (index % this.board_width > 0)
+                n.push(index - 1);
+            if (index % this.board_width < this.board_width - 1)
+                n.push(index + 1);
+            if (index < this.fields.length - this.board_width)
+                n.push(index + this.board_width);
+            return n;
         },
 
         // where player click on field
@@ -64,30 +93,16 @@ class Field {
                 return neighbors;
             }
             return false;
-        },
-
-        // return array of index neighbors
-        getNeighbors : function(index) {
-            let n = [];
-            if (index >= this.board_width)
-                n.push(index - this.board_width);
-            if (index % this.board_width > 0)
-                n.push(index - 1);
-            if (index % this.board_width < this.board_width - 1)
-                n.push(index + 1);
-            if (index < this.fields.length - this.board_width)
-                n.push(index + this.board_width);
-            return n;
         }
+
     };
 
     let view = {
-        board : Element,
         fieldsNods : [],
 
         // initialize board game
         init : function(b_height, b_width) {
-            this.board = document.querySelector('.board');
+            board = document.querySelector('.board');
             let index = 0;
             //console.log(b_width, b_height);
             for (let row = 0; row < b_height; row++) {
@@ -101,12 +116,36 @@ class Field {
                     rowNode.append(fieldNode);
                     this.fieldsNods.push(fieldNode);
                 }
-                this.board.append(rowNode);  
+                board.append(rowNode);  
             }
             //console.log(this.fieldsNods);
         },
-        repanitField : function(index, f) {
-            let fNode = this.fieldsNods[index];
+        repanitFields : function(fields, castels) {
+            
+            for (let index = 0; index < this.fieldsNods.length; index++) {
+                // castel
+                let list = 'field';
+                let text = "";
+                if (castels[index] > 0) {
+                    text += castels[index];
+                    list += ' castel1';
+                } 
+                if (castels[index] < 0) {
+                    text += castels[index] * -1;
+                    list += ' castel2';
+                } 
+                if (fields[index] > 0) {
+                    text += "," + fields[index];
+                    list += ' player1';
+                } 
+                if (fields[index] < 0) {
+                    text += "," + fields[index] * - 1;
+                    list += ' player2';
+                }
+                this.fieldsNods[index].classList = list;
+                this.fieldsNods[index].innerText = text;
+            }
+            return;
             // background
             if (fNode.classList.contains('player1') && f.owner != 1 ){
                 fNode.classList.remove('player1');
@@ -141,14 +180,21 @@ class Field {
             console.log('start');
         },
         clickField : function(e){
-            // if pasible update field and neighbors
             let index = Number(e.target.id);
+
+            model.owners[index] += model.player;
+            model.player *= -1;
+            view.repanitFields(model.getAffects(model.owners), model.owners);
+
+            // if pasible update field and neighbors
+            /*
             let neighbors = model.updateField(index);
             if ( neighbors ){
                 console.log(neighbors);
                 view.repanitField(index, model.fields[index]);
                 neighbors.forEach((i) => view.repanitField(i, model.fields[i]));
             }
+            */
         }
 
     };
